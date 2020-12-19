765de1ccb8a6f238d86ddfae759a75e444e60a23
MURA199918 <muralisaranr18@gmail.com>
2020-12-19 19:22:55 +0530

    [Murali]Refactored to get top batting averages of cricketers

--- src/main/java/IPLAnalysis.java
+++ src/main/java/IPLAnalysis.java
@@ -1,5 +1,73 @@
+import com.google.gson.Gson;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class IPLAnalysis {
-    public static void main(String[] args) {
-        System.out.println("..........Welcome to IPL Analysis Program.............");
+
+    public static final String TOP_BATTING_AVG_FILE = "C:\\Users\\mural\\IdeaProjects\\IPL_Analysis_Problem\\src\\test\\resources\\TopBattingAverage.json";
+    public static List<IPLBatsman> IplBatsmanData;
+    public static List<IPLBowler> IplBowlerData;
+
+
+    public int loadIplBatsmanStat(String csvFilePath) throws IPLAnalysisException {
+        try(Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+            CSVReader csvReader = new CSVReader(reader);){
+
+            IplBatsmanData = getCSVFileList(reader,IPLBatsman.class);
+            return IplBatsmanData.size();
+        } catch (IOException | RuntimeException e ) {
+            throw new IPLAnalysisException(e.getMessage(),IPLAnalysisException.ExceptionType.IPL_FILE_PROBLEM);
+        }
+    }
+
+    public int loadIplBowlerStat(String csvFilePath) throws IPLAnalysisException {
+        try(Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+            CSVReader csvReader = new CSVReader(reader);){
+
+            IplBowlerData = getCSVFileList(reader,IPLBowler.class);
+            return IplBowlerData.size();
+        } catch (IOException| RuntimeException e ) {
+            throw new IPLAnalysisException(e.getMessage(),IPLAnalysisException.ExceptionType.IPL_FILE_PROBLEM);
+        }
+    }
+
+    public <E> List getCSVFileList(Reader reader, Class csvClass)  {
+
+        CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+        csvToBeanBuilder.withType(csvClass);
+        csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+        CsvToBean<E> csvToBean = csvToBeanBuilder.build();
+        return csvToBean.parse();
+    }
+
+
+    public void getTopBattingAverageData() throws IOException {
+        Comparator com = new Comparator() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                IPLBatsman b1 = (IPLBatsman)o1;
+                IPLBatsman b2 = (IPLBatsman)o2;
+                if(b1.getAverage()>b2.getAverage())
+                    return -3;
+                else
+                    return 3;
+            }
+        };
+        Collections.sort(IplBatsmanData,com);
+        Gson gson = new Gson();
+        String json =gson.toJson(IplBatsmanData);
+        FileWriter fileWriter = new FileWriter(TOP_BATTING_AVG_FILE);
+        fileWriter.write(json);
+        fileWriter.close();
     }
 }
--- src/test/java/IPLAnalysisTest.java
+++ src/test/java/IPLAnalysisTest.java
@@ -1,3 +1,31 @@
+import com.jarfile.CSVBuilderException;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
 public class IPLAnalysisTest {
+
+    public static final String BATSMAN_PATH = "C:\\Users\\mural\\IdeaProjects\\IPL_Analysis_Problem\\src\\test\\resources\\IPL2019FactsheetMostRuns.csv";
+    public static final String BOWLER_PATH = "C:\\Users\\mural\\IdeaProjects\\IPL_Analysis_Problem\\src\\test\\resources\\IPL2019FactsheetMostWkts.csv";
+
+    @Test
+    public void loadingGivenCSVAndCheckingNoOfEntries() throws IPLAnalysisException, IOException, CSVBuilderException {
+        IPLAnalysis iplAnalysis = new IPLAnalysis();
+        int noOfEntriesBatsman = iplAnalysis.loadIplBatsmanStat(BATSMAN_PATH);
+        Assert.assertEquals(101,noOfEntriesBatsman);
+        int noOfEntriesBowler = iplAnalysis.loadIplBowlerStat(BOWLER_PATH);
+        Assert.assertEquals(99,noOfEntriesBowler);
+
+    }
+
+    @Test
+    public void givenIPLRunsData_ShouldReturn_TopBattingAverages_ofCricketers() throws IPLAnalysisException, IOException {
+        IPLAnalysis iplAnalysis = new IPLAnalysis();
+        iplAnalysis.loadIplBatsmanStat(BATSMAN_PATH);
+        iplAnalysis.getTopBattingAverageData();
+        Assert.assertEquals("MS Dhoni",IPLAnalysis.IplBatsmanData.get(0).getPlayer());
+        System.out.println("Answer found");
+    }
 
 }
